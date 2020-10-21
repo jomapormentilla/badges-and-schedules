cc8841dc980f70193c9a3649eba1044e2f4775ad
jomapormentilla <jomapormentilla@gmail.com>
2020-10-21 12:36:04 -0400

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,38 @@
 # Write your code here.
+
+def badge_maker( name )
+    "Hello, my name is #{ name }."
+end
+
+def batch_badge_creator( array )
+    arr = []
+    array.each do |name|
+        arr.push("Hello, my name is #{ name }.")
+    end
+
+    arr
+end
+
+def assign_rooms( array )
+    arr = []
+    counter = 1
+    array.each do |speaker|
+        arr.push("Hello, #{ speaker }! You'll be assigned to room #{ counter }!")
+        counter += 1
+    end
+
+    arr
+end
+
+def printer( array )
+    badges = batch_badge_creator( array )
+    rooms = assign_rooms( array )
+
+    badges.each do |badge|
+        puts badge
+    end
+
+    rooms.each do |room|
+        puts room
+    end
+end
